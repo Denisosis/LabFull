2baa1963cb9200b436beb4725b57b728dccf2328
Denisosis <91203080+Denisosis@users.noreply.github.com>
2021-12-25 12:09:28 +0300

    LabFull

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -18,62 +18,62 @@ import static org.junit.Assert.assertNotEquals;
 public class AppTest {
     private static final Logger log = LogManager.getLogger(AppTest.class);
 
-    dataProviders.mySql.DataProvider dataProvider = new dataProviders.mySql.DataProvider();
-    Book new_book_1 = new Book( 1, "Taras Bulba", "Russia");
-    Book new_book_2 = new Book( 2, "Books English", "UK");
+        dataProviders.mySql.DataProvider dataProvider = new dataProviders.mySql.DataProvider();
+        Book new_book_1 = new Book( 1, "Taras Bulba", "Russia");
+        Book new_book_2 = new Book( 2, "Books English", "UK");
 
-    @Before
-    public void insert(){
-        drop();
-        dataProvider.insert(new_book_1);
-        dataProvider.insert(new_book_2);
-    }
+        @Before
+        public void insert(){
+            drop();
+            dataProvider.insert(new_book_1);
+            dataProvider.insert(new_book_2);
+        }
 
-    public void drop(){
-        DataProvider.dropData();
-    }
+        public void drop(){
+            DataProvider.dropData();
+        }
 
-    @Test
-    public void getByIdPositive(){
-        Book byId = dataProvider.getById(new_book_1.getId());
-        assertEquals(new_book_1.getId(), byId.getId());
-    }
+        @Test
+        public void getByIdPositive(){
+            Book byId = dataProvider.getById(new_book_1.getId());
+            assertEquals(new_book_1.getId(), byId.getId());
+        }
 
-    @Test
-    public void getByIdNegative(){
-        Book byId = dataProvider.getById(System.currentTimeMillis());
-        assertNotEquals(new_book_1.getId(), byId.getId());
-    }
+        @Test
+        public void getByIdNegative(){
+            Book byId = dataProvider.getById(System.currentTimeMillis());
+            assertNotEquals(new_book_1.getId(), byId.getId());
+        }
 
-    @Test
-    public void select(){
-        List<Book> entities;
-        entities = dataProvider.selectBooks();
-        assertEquals(2, entities.size());
-    }
+        @Test
+        public void select(){
+            List<Book> entities;
+            entities = dataProvider.selectBooks();
+            assertEquals(2, entities.size());
+        }
 
-    @Test
-    public void delete(){
-        Book forDelete = new Book();
-        dataProvider.delete(2);
-        assertEquals(forDelete, dataProvider.getById(2));
-    }
+        @Test
+        public void delete(){
+            Book forDelete = new Book();
+            dataProvider.delete(2);
+            assertEquals(forDelete, dataProvider.getById(2));
+        }
 
-    @Test
-    public void updatePositive(){
-        Book newEntity = new Book(2,"newBookName", "newCountry");
-        dataProvider.update(newEntity);
-        assertEquals(newEntity, dataProvider.getById(newEntity.getId()));
-    }
+        @Test
+        public void updatePositive(){
+            Book newEntity = new Book(2,"newBookName", "newCountry");
+            dataProvider.update(newEntity);
+            assertEquals(newEntity, dataProvider.getById(newEntity.getId()));
+        }
 
-    @Test
-    public void updateNegative(){
-        Book newEntity = new Book(System.currentTimeMillis(), "newBookName", "newCountry");
-        dataProvider.update(newEntity);
-        assertNotEquals(newEntity, dataProvider.getById(newEntity.getId()));
-    }
-    /*
-    @Test
+        @Test
+        public void updateNegative(){
+            Book newEntity = new Book(System.currentTimeMillis(), "newBookName", "newCountry");
+            dataProvider.update(newEntity);
+            assertNotEquals(newEntity, dataProvider.getById(newEntity.getId()));
+        }
+
+  /*  @Test
     public void insert(){
         Book newBook = new Book("Taras Bulba", "Russia");
         Book newBook1 = new Book("Bet Book", "UK");
